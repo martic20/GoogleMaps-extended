db80f0dd50cd59bb570ce5da0d4e92d2be16b440
Martí <martic20@gmail.com>
2018-04-16 19:35:13 +0200

    changed map location provider

--- app/src/main/java/martic20/googlemaps/MapsActivity.java
+++ app/src/main/java/martic20/googlemaps/MapsActivity.java
@@ -12,6 +12,9 @@ import android.os.Bundle;
 import android.widget.Toast;
 import android.Manifest;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,34 +22,116 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+/*
+A partir del ejercicio de Geolocalización que realizaste,
+vamos a realizar un pequeño juego, sobre la ciudad de Barcelona
+(en el ejercicio anterior, mostraba en el mapa los 4/5 lugares de interés de Barcelona).
+Nuestro juego, tomará la ubicación actual, y cuando el usuario llegue a la
+ubicación marcada en el mapa (a partir de las coordenadas),
+nos mostrará una pregunta sobre algo que esté en esa ubicación.
+En caso de acertarla, se marcará esa ubicación como resuelta.
+Cuando el usuario consiga los 4/5 lugares marcados, saldrá un mensaje de juego completado.
+
+ */
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+
+    private GoogleApiClient googleApiClient;
+
+    private static final int LOCATION_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        checkLocationandAddToMap();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
 
-        LocationManager gestorLoc = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    @Override
+    public void onConnectionSuspended(int i) {
 
-        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
-        } else {
-            try {
-                gestorLoc.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
-            } catch (Exception e) {
-                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
-            }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkLocationandAddToMap();
+                } else
+                    Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
+                break;
+
         }
+
+    }
+
+    private void checkLocationandAddToMap() {
+//Checking if the user has granted the permission
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//Requesting the Location permission
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            return;
+        }
+
+//Fetching the last known location using the Fus
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+//MarkerOptions are used to create a new Marker.You can specify location, title etc with MarkerOptions
+        MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You are Here");
+
+//Adding the created the marker on the map
+        mMap.addMarker(markerOptions);
+
     }
 
 
+/*
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -54,63 +139,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(41.395253, 2.161658);
         mMap.addMarker(new MarkerOptions().position(sydney).title("La pedrera"));
-        
+
         LatLng s2 = new LatLng(41.403859, 2.174367);
         mMap.addMarker(new MarkerOptions().position(s2).title("Sagrada Familia"));
-        
+
         LatLng s3 = new LatLng(41.412005, 2.226315);
         mMap.addMarker(new MarkerOptions().position(s3).title("Parc del fórum"));
-        
+
         LatLng s4 = new LatLng(41.391846, 2.164818);
-        mMap.addMarker(new MarkerOptions().position(s4).title("La pedrera"));
-        
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera( CameraUpdateFactory.zoomTo( 20.0f ) );
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        String text = "Posició actual:\n" +
-                "Latitud = " + location.getLatitude() + "\n" +
-                "Longitud = " + location.getLongitude();
-
+        mMap.addMarker(new MarkerOptions().position(s4).title("Casa Batlló"));
 
     }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(getApplicationContext(),
-                "GPS desactivat per l'usuari",
-                Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        Toast.makeText(getApplicationContext(),
-                "GPS habilitat per l'usuari",
-                Toast.LENGTH_LONG).show();
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        String missatge = "";
-        switch (status) {
-            case LocationProvider.OUT_OF_SERVICE:
-                missatge = "GPS status: Out of service";
-                break;
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                missatge = "GPS status: Temporarily unavailable";
-                break;
-            case LocationProvider.AVAILABLE:
-                missatge = "GPS status: Available";
-                break;
-        }
-
-        Toast.makeText(getApplicationContext(),
-                missatge,
-                Toast.LENGTH_LONG).show();
-    }
+*/
 }
